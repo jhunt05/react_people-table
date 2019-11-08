73f918d242db312a8bf7cebdee5914ad4d5f0cf3
Viktoriia <jhunt@ukr.net>
2019-11-08 11:58:34 +0200

    Updates

---
+++ precache-manifest.97472b429fa1b4cea6e1e2b6e7f3d975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f450bcbca464dcd4ccc0ec497d8af5ce",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "63d11d39edadd1a6a1f6",
+    "url": "/react_people-table/static/css/main.5a821f8b.chunk.css"
+  },
+  {
+    "revision": "64a9bd5b5a889be3e3d3",
+    "url": "/react_people-table/static/js/2.127d0378.chunk.js"
+  },
+  {
+    "revision": "63d11d39edadd1a6a1f6",
+    "url": "/react_people-table/static/js/main.a87debaa.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
